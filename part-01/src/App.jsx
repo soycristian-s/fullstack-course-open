9d6b3935767260ff142f17647e117615b48500fe
CrSierra <cristianandressm@gmail.com>
2025-01-06 20:11:52 -0500

    Initialize React and Vite project with basic configuration and example components

---
+++ part-01/src/App.jsx
@@ -0,0 +1,70 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+import PropTypes from 'prop-types'
+
+const Hello = ({name}) => {
+
+  return (
+    <div>
+      <p>Saludooos! {name} </p>
+    </div>
+  )
+}
+
+Hello.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+const App = () => {
+  const now = Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <Hello name='Jorge' />
+      <Hello name='Cris' />
+    </div>
+  )
+}
+
+export default App
