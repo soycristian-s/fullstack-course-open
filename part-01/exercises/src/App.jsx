ebb01cf7ce9e8b127f6d35629dc1e43207719766
CrSierra <cristianandressm@gmail.com>
2025-01-12 15:37:03 -0500

    End exercise 1.2.

--- part-01/exercises/src/App.jsx
+++ part-01/exercises/src/App.jsx
@@ -1,3 +1,5 @@
+import { Children } from "react";
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -23,48 +25,60 @@ const App = () => {
     // </div>}
     <div>
       <Header show_title={course} />
-      <Content prop={{partes:[part1, part2, part3], ejercicios:[exercises1, exercises2, exercises3]}}/>
+      <Content
+        prop={{
+          partes: [part1, part2, part3],
+          ejercicios: [exercises1, exercises2, exercises3],
+        }}
+      />
       <Total ejercicios={[exercises1, exercises2, exercises3]} />
     </div>
   );
 };
 
-const Header = ({show_title}) => {
+const Header = ({ show_title }) => {
   return (
-  <>
-    <h1>{show_title} </h1>
-  </>
-)};
+    <>
+      <h1>{show_title} </h1>
+    </>
+  );
+};
 
+const Content = ({
+  prop: {
+    partes: [part1, part2, part3],
+    ejercicios: [exercises1, exercises2, exercises3],
+  },
+}) => {
+  return (
+    <>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </>
+  );
+};
 
-const Content = ({ prop: { partes, ejercicios } }) => {
-  let [part1, part2, part3] = partes
-  let [exercises1, exercises2, exercises3] = ejercicios
+const Part = ({ part, exercise }) => {
+  return (
+    <>
+      <p>
+        {" "}
+        {part} {exercise}{" "}
+      </p>
+    </>
+  );
+};
+
+const Total = ({ ejercicios }) => {
+  let total = ejercicios.reduce((acc, current) => {
+    return acc + current;
+  }, 0);
 
   return (
     <>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p> 
+      <p>Number of exercises {total}</p>
     </>
-)
-}
-
-const Total = ({ejercicios}) => {
-  let total = ejercicios.reduce((acc, current)=>{ 
-    return acc + current
-  },0)
-  
-  return (
-    <>
-    <p>Number of exercises {total}</p>
-    </>
-  )
-}
+  );
+};
 export default App;
