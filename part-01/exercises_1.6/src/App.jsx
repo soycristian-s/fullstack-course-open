50f5666c696057fab7e12d08ca6e75f294742fdb
CrSierra <cristianandressm@gmail.com>
2025-03-23 12:09:33 -0500

    End exercise 1.7

--- part-01/exercises_1.6/src/App.jsx
+++ part-01/exercises_1.6/src/App.jsx
@@ -6,6 +6,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const handleAverage = () => {
+  }
+
+  const totalComents = good+neutral+bad
+  const averageComents = (good-bad)/totalComents
+  const positiveComents = good/totalComents*100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -15,6 +22,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {totalComents}</p>
+      <p>Average score{averageComents}</p>
+      <p>Positive score{positiveComents} %</p>
     </div>
   );
 };
