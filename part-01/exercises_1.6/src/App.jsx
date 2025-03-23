d234b75beffb25fcf6134698908a582e9949adc4
CrSierra <cristianandressm@gmail.com>
2025-03-23 18:20:38 -0500

    End exercise 1.10

--- part-01/exercises_1.6/src/App.jsx
+++ part-01/exercises_1.6/src/App.jsx
@@ -3,17 +3,23 @@ import { useState } from "react";
 const Statistics = ({ comments: { good, neutral, bad } }) => {
   const totalComents = good + neutral + bad;
   const averageComents = (good - bad) / totalComents || 0;
-  const positiveComents = (good / totalComents) * 100 || 0;
+  const positiveComents = (good / totalComents) * 100 || 0 + " %";
 
   if (totalComents !== 0) {
     return (
       <>
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
-        <p>All {totalComents}</p>
-        <p>Average score {averageComents}</p>
-        <p>Positive score {positiveComents} %</p>
+        <StatisticLine text={"Good"} value={good}></StatisticLine>
+        <StatisticLine text={"Neutral"} value={neutral}></StatisticLine>
+        <StatisticLine text={"Bad"} value={bad}></StatisticLine>
+        <StatisticLine text={"All"} value={totalComents}></StatisticLine>
+        <StatisticLine
+          text={"Average score"}
+          value={averageComents}
+        ></StatisticLine>
+        <StatisticLine
+          text={"Positive score"}
+          value={positiveComents}
+        ></StatisticLine>
       </>
     );
   } else {
@@ -21,8 +27,19 @@ const Statistics = ({ comments: { good, neutral, bad } }) => {
   }
 };
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
+const Button = ({ handler, text }) => {
+  return <button onClick={handler}>{text}</button>;
+};
+
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -30,9 +47,9 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Button handler={() => setGood(good + 1)} text={"good"}></Button>
+      <Button handler={() => setNeutral(neutral + 1)} text={"neutral"}></Button>
+      <Button handler={() => setBad(bad + 1)} text={"bad"}></Button>
       <h1>Stadistics</h1>
       <Statistics comments={{ good, neutral, bad }}></Statistics>
     </div>
