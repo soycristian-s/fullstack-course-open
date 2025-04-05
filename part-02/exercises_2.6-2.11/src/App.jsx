3134d5f2775c358b851bdd7c2f4ebdd5026f7a3e
CrSierra <cristianandressm@gmail.com>
2025-04-04 22:04:28 -0500

    End exercise 2.11

--- part-02/exercises_2.6-2.11/src/App.jsx
+++ part-02/exercises_2.6-2.11/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const FilterSeccion = ({ inputs: [filterBy, handleFilterBy] }) => {
   return (
@@ -31,7 +32,7 @@ const PersonLineForm = ({ inputs: [value, handler], label }) => {
 
 const PersonDisplay = ({ personList }) => {
   return personList.map((person) => (
-    <div key={person.name}>
+    <div key={person.id}>
       <strong>Name:</strong> {person.name} <strong>Number:</strong>{" "}
       {person.number}
     </div>
@@ -39,16 +40,18 @@ const PersonDisplay = ({ personList }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterBy, setFilterBy] = useState("");
   const [showPerson, setShowPerson] = useState([...persons]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setShowPerson(response.data);
+    });
+  }, []);
 
   const handleNameChange = (e) => setNewName(e.target.value);
   const handleNumberChange = (e) => setNewNumber(e.target.value);
